dc56a67c66a1b9aa341629c20bc2dfe643aa6e3b
Adrian Buchwald <adrian.buchwald@gmail.com>
2024-02-06 15:04:23 +0100

    added cifar 40+ dataset for testing

--- main_model/dataset.py
+++ main_model/dataset.py
@@ -30,6 +30,12 @@ class ImageDataset(Dataset):
             self.mean = [0.4914, 0.4822, 0.4465]
             self.std = [0.2023, 0.1994, 0.2010]
             self.image_key = 'img'
+        elif dataset_type == 'cifar_more':
+            image_dataset_huggingface_more = load_dataset('cifar100', split=split)
+            image_dataset_huggingface = load_dataset('cifar10', split=split)
+            self.mean = [0.4914, 0.4822, 0.4465]
+            self.std = [0.2023, 0.1994, 0.2010]
+            self.image_key = 'img'
         else:
             raise ValueError('Dataset not specified correctly!')
 
@@ -43,23 +49,30 @@ class ImageDataset(Dataset):
         if is_close:
             self.image_dataset = image_dataset_huggingface.filter(
                 lambda data_point: data_point['label'] <= chosen_classes)
+            self.label_name = 'label'
+        elif dataset_type == 'cifar_more' and not is_close:
+            self.image_dataset = image_dataset_huggingface_more.filter(
+                lambda data_point: data_point['fine_label'] <= 40)
+            self.label_name = 'fine_label'
         else:
             self.image_dataset = image_dataset_huggingface.filter(
                 lambda data_point: data_point['label'] > chosen_classes)
+            self.label_name = 'label'
+
 
     def __len__(self):
         return len(self.image_dataset)
 
     def __getitem__(self, idx):
         img = self.image_dataset[idx][self.image_key]
-        label = self.image_dataset[idx]['label']
+        label = self.image_dataset[idx][self.label_name]
 
         # find a non match image in the dataset
         label_matched = True
         non_match_idx = 0
         while label_matched:
             non_match_idx = int(np.random.random_integers(0, len(self.image_dataset)) - 1)
-            non_match_label = self.image_dataset[non_match_idx]['label']
+            non_match_label = self.image_dataset[non_match_idx][self.label_name]
             if non_match_label != label:
                 label_matched = False
         non_match_img = self.image_dataset[non_match_idx][self.image_key]
@@ -70,7 +83,7 @@ class ImageDataset(Dataset):
         return img, label, non_match_img, non_match_label
 
     def num_classes(self):
-        return len(self.image_dataset.unique('label'))
+        return len(self.image_dataset.unique(self.label_name))
 
     def reverse_normalization(self, image):
         inv_transform = v2.Normalize(-np.array(self.mean)/np.array(self.std),
